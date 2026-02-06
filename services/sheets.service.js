83642da7a0aa1464cc3b39f10e48c2096e13dc01
Gaurav Shukla <gauravshukla0008@gmail.com>
2026-02-06 15:08:36 +0800

    updated cred in env

--- services/sheets.service.js
+++ services/sheets.service.js
@@ -1,5 +1,4 @@
 const { google } = require('googleapis');
-const path = require('path');
 
 class SheetsService {
   constructor() {
@@ -13,9 +12,22 @@ class SheetsService {
     if (this.initialized) return;
 
     try {
-      // Initialize Google Auth
+      // Initialize Google Auth using environment variables
+      const credentials = {
+        type: 'service_account',
+        project_id: process.env.GOOGLE_PROJECT_ID,
+        private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+        private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+        token_uri: 'https://oauth2.googleapis.com/token',
+        auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+        client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL
+      };
+
       this.auth = new google.auth.GoogleAuth({
-        keyFile: path.join(__dirname, '../config/google-credentials.json'),
+        credentials: credentials,
         scopes: ['https://www.googleapis.com/auth/spreadsheets']
       });
 
